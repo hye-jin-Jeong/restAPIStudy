4e022e8f6567e53f8a500da76ca827f9a3417b7b
hyejin <hjjeong@withbrother.co.kr>
2022-05-13 13:53:45 +0900

    refactor: api endpoint injection 개선

--- routes/api.php
+++ routes/api.php
@@ -28,8 +28,14 @@ use App\Article;
 //     return 204;
 // });
 
+// Route::get('articles', 'ArticleController@index');
+// Route::get('articles/{id}', 'ArticleController@show');
+// Route::post('articles', 'ArticleController@store');
+// Route::put('articles/{id}', 'ArticleController@update');
+// Route::delete('articles/{id}', 'ArticleController@delete');
+
 Route::get('articles', 'ArticleController@index');
-Route::get('articles/{id}', 'ArticleController@show');
+Route::get('articles/{article}', 'ArticleController@show');
 Route::post('articles', 'ArticleController@store');
-Route::put('articles/{id}', 'ArticleController@update');
-Route::delete('articles/{id}', 'ArticleController@delete');
+Route::put('articles/{article}', 'ArticleController@update');
+Route::delete('articles/{article}', 'ArticleController@delete');
