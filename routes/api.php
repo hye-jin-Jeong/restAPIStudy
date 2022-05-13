e05c7e6f603ce414d889763d6cd0cc3e8655aec5
hyejin <hjjeong@withbrother.co.kr>
2022-05-13 17:29:45 +0900

    feat: logout post 날리면 api_token 지우기& 로긴안된 유저 접근 막기

--- routes/api.php
+++ routes/api.php
@@ -34,11 +34,20 @@ use App\Article;
 // Route::put('articles/{id}', 'ArticleController@update');
 // Route::delete('articles/{id}', 'ArticleController@delete');
 
-Route::get('articles', 'ArticleController@index');
-Route::get('articles/{article}', 'ArticleController@show');
-Route::post('articles', 'ArticleController@store');
-Route::put('articles/{article}', 'ArticleController@update');
-Route::delete('articles/{article}', 'ArticleController@delete');
+
 
 Route::post('register', 'Auth\RegisterController@register');
 Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout');
+
+Route::middleware('auth:api')->get('/user', function(Request $request){
+    return $request->user();
+});
+
+Route::group(['middleware' => 'auth:api'], function(){
+    Route::get('articles', 'ArticleController@index');
+    Route::get('articles/{article}', 'ArticleController@show');
+    Route::post('articles', 'ArticleController@store');
+    Route::put('articles/{article}', 'ArticleController@update');
+    Route::delete('articles/{article}', 'ArticleController@delete');
+});
