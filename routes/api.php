d04f03378c51a21cdfec92e9d017cead4d254665
hyejin <hjjeong@withbrother.co.kr>
2022-05-13 13:28:39 +0900

    refactor: controller 로 엔드포인트 분리

--- routes/api.php
+++ routes/api.php
@@ -3,27 +3,33 @@
 use Illuminate\Http\Request;
 use App\Article;
 
-Route::get('articles', function(){
-    return Article::all();
-});
+// Route::get('articles', function(){
+//     return Article::all();
+// });
 
-Route::get('articles/{id}', function($id){
-    return Article::find($id);
-});
+// Route::get('articles/{id}', function($id){
+//     return Article::find($id);
+// });
 
-Route::post('articles', function(Request $request){
-    return Article::create($request->all);
-});
+// Route::post('articles', function(Request $request){
+//     return Article::create($request->all);
+// });
 
-Route::put('articles/{id}', function(Request $request, $id){
-    $article = Article::findOrFail($id);
-    $article->update($request->all());
+// Route::put('articles/{id}', function(Request $request, $id){
+//     $article = Article::findOrFail($id);
+//     $article->update($request->all());
 
-    return $article;
-});
+//     return $article;
+// });
 
-Route::delete('articles/{id}', function($id){
-    Article::find($id)->delete();
+// Route::delete('articles/{id}', function($id){
+//     Article::find($id)->delete();
 
-    return 204;
-});
+//     return 204;
+// });
+
+Route::get('articles', 'ArticleController@index');
+Route::get('articles/{id}', 'ArticleController@show');
+Route::post('articles', 'ArticleController@store');
+Route::put('articles/{id}', 'ArticleController@update');
+Route::delete('articles/{id}', 'ArticleController@delete');
